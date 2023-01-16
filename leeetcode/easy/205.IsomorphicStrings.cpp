76c83b1321cb33f4fe7404000208f88f1ab3e4b6
meow <some>
2023-01-16 05:23:38 +0300

    fix 205 isIsomorphic

--- leeetcode/easy/205.IsomorphicStrings.cpp
+++ leeetcode/easy/205.IsomorphicStrings.cpp
@@ -8,32 +8,31 @@ using namespace std;
 class Solution {
  public:
   bool isIsomorphic(string s, string t) {
-    auto str = max(s, t);
-    if (s.size() != t.size()) {
-      return false;
-    };
-    map<char, char> s_abc;
-    map<char, char> t_abc;
-    for (size_t i = 0; i < str.size(); i++) {
+    vector<char> s_abc(300,0);
+    vector<char> t_abc(300,0);
+
+    for (size_t i = 0; i < s.size(); i++) {
       if (s_abc[s[i]] != 0) {
         if (s_abc[s[i]] != t[i]) return false;
       }
 
       if (t_abc[t[i]] != 0) {
-        if (t_abc[s[i]] != s[i]) return false;
+        if (t_abc[t[i]] != s[i]) return false;
       }
 
       s_abc[s[i]] = t[i];
       t_abc[t[i]] = s[i];
     }
+
     return true;
   }
 };
 
 int main(void) {
   Solution sol;
-  string kek = "jopa";
-  string kek2 = "jopo";
+  string kek = "egg";
+  string kek2 = "add";
   cout << sol.isIsomorphic(kek, kek2);
+  cout << "jopa";
   return 0;
 }
