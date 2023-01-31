958f4bbd6d32cdde9034d54bf9d15d3ae5cf5ac2
meow <some>
2023-02-01 00:52:22 +0300

    add read/write gorutines

--- interviews/yadro/main.go
+++ interviews/yadro/main.go
@@ -1,46 +1,107 @@
 package main
 
-// slice keyboard.
-var keyboard = [][]byte{ //nolint:gochecknoglobals // it's a demo.
-	{'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'},
-	{'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'},
-	{'z', 'x', 'c', 'v', 'b', 'n', 'm'},
-}
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"runtime"
+	"sync"
+)
 
-var keyboardMap = map[byte][]int8{ //nolint:gochecknoglobals // it's a demo.
-	'q': {0, 0},
-	'w': {0, 1},
-	'e': {0, 2},
-	'r': {0, 3},
-	't': {0, 4},
-	'y': {0, 5},
-	'u': {0, 6},
-	'i': {0, 7},
-	'o': {0, 8},
-	'p': {0, 9},
-	'a': {1, 0},
-	's': {1, 1},
-	'd': {1, 2},
-	'f': {1, 3},
-	'g': {1, 4},
-	'h': {1, 5},
-	'j': {1, 6},
-	'k': {1, 7},
-	'l': {1, 8},
-	'z': {2, 0},
-	'x': {2, 1},
-	'c': {2, 2},
-	'v': {2, 3},
-	'b': {2, 4},
-	'n': {2, 5},
-	'm': {2, 6},
-}
+// get english words from site https://www.ef.com/wwen/english-resources/english-vocabulary/top-3000-words/
 
 func main() {
-	v := keyboardMap['q']
-	x, y := v[0], v[1]
-	println(string(keyboard[x][y]))
+	file, err := os.Open("file.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// file size
+	fileInfo, err := file.Stat()
+	if err != nil {
+		println(err)
+		return
+	}
+
+	sizeOfFile := fileInfo.Size()
+	proc := runtime.GOMAXPROCS(0) - 1
+	kbPerGoroutine := int(math.Ceil(float64(sizeOfFile) / float64(proc) / 1024))
+
+	linesPool := sync.Pool{New: func() interface{} {
+		lines := make([]byte, kbPerGoroutine*1024)
+		return lines
+	}}
+
+	newWordch := make(chan string, 100)
+
+	var wg2 sync.WaitGroup
+
+	wg2.Add(1)
+
+	go func() {
+		i := 0
+		for word := range newWordch {
+			i++
+			println(i, word)
+		}
+
+		wg2.Done()
+		println("channel closed")
+	}()
+
+	var wg sync.WaitGroup
+
+	reader := bufio.NewReader(file)
+
+loop:
+	for i := 0; i < proc; i++ {
+		buf, ok := linesPool.Get().([]byte)
+		if !ok {
+			println(buf)
+			panic("linesPool.Get() returned not []byte")
+		}
+
+		bytesNums, err := reader.Read(buf)
+
+		switch {
+		case err == io.EOF && bytesNums == 0:
+			break loop
+		case err != nil:
+			fmt.Println(err)
+			break loop
+		}
+
+		buf = buf[:bytesNums]
+		// if was readed half of the word.
+		endOfWord, err := reader.ReadBytes('\n')
+		if err != io.EOF {
+			buf = append(buf, endOfWord...)
+		}
+
+		wg.Add(1)
+
+		go func() {
+			readWordsFromChunk(&buf, &linesPool, newWordch)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	close(newWordch)
+	wg2.Wait()
+	println("all goroutines done")
 }
 
-func bfs_search(x, y, target int8) {
+func readWordsFromChunk(buffer *[]byte, chankPool *sync.Pool, newWordch chan<- string) {
+	scanner := bufio.NewScanner(bytes.NewReader(*buffer))
+	chankPool.Put(buffer)
+
+	for scanner.Scan() {
+		word := scanner.Text()
+		newWordch <- word
+	}
 }
